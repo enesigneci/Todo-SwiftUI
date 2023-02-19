d953562fd3e7665c0488c98bd7c9eb2861d04665
Enes İğneci <enes_igneci@epam.com>
2023-02-20 00:54:09 +0300

    Initial Commit

---
+++ Start/StartApp.swift
@@ -0,0 +1,17 @@
+//
+//  StartApp.swift
+//  Start
+//
+//  Created by Enes_Igneci on 20.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct StartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
